a34a6873b844dc15e171aa7754867ffb7ce3eda1
KINGLY <ratanaly92@gmail.com>
2025-10-02 20:35:15 +0700

    add enemy and gold

---
+++ Assets/Scripts/GoldRotate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldRotate : MonoBehaviour
+{
+    [SerializeField] private float speed = 100;
+
+    private UIManager uiManager = null;
+
+    private void Awake()
+    {
+        uiManager = FindFirstObjectByType<UIManager>();
+    }
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log($"{gameObject.name} Hit Player");
+            if (uiManager != null)
+                uiManager.RefreshGoldHitCount();
+            Destroy(gameObject);
+        }
+    }
+}
